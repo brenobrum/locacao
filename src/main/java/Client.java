ba825f9556cfbf3fa44abf434cd98e8bb22e4dcf
brenobrum <breno.brum2001@gmail.com>
2022-05-15 16:49:55 -0300

    add rent methods

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Client {
+public class Client {
+    private String cpf;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Client(String cpf, String name, String address, String phone, String email) {
+        this.cpf = cpf;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/Rent.java
+++ src/main/java/Rent.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class rent {
+import java.sql.Time;
+import java.util.Date;
+
+public class Rent {
+    private Date rentDate;
+    private Time rentTime;
+    private Date returnDate;
+    private Time returnTime;
+    private Long rentMileage;
+    private Long returnMileage;
+    private double bailValue;
+    private String rentValue;
+    private Boolean returned;
+
+    public Rent(Date rentDate, Time rentTime) {
+        this.rentDate = rentDate;
+        this.rentTime = rentTime;
+        this.returned = false;
+    }
+
+    public Date getRentDate() {
+        return rentDate;
+    }
+
+    public Time getRentTime() {
+        return rentTime;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Time getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(Time returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public Long getRentMileage() {
+        return rentMileage;
+    }
+
+    public void setRentMileage(Long rentMileage) {
+        this.rentMileage = rentMileage;
+    }
+
+    public Long getReturnMileage() {
+        return returnMileage;
+    }
+
+    public void setReturnMileage(Long returnMileage) {
+        this.returnMileage = returnMileage;
+    }
+
+    public double getBailValue() {
+        return bailValue;
+    }
+
+    public void setBailValue(double bailValue) {
+        this.bailValue = bailValue;
+    }
+
+    public String getRentValue() {
+        return rentValue;
+    }
+
+    public void setRentValue(String rentValue) {
+        this.rentValue = rentValue;
+    }
+
+    public Boolean getReturned() {
+        return returned;
+    }
+
+    public void setReturned(Boolean returned) {
+        this.returned = returned;
+    }
 }
