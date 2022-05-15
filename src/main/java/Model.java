1603e646932cc8f18410ca7a2361a44beca5ae74
Math <tetei-2007@hotmail.com>
2022-05-15 16:54:44 -0300

    Brand, Model and Vehicle updates

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,3 +1,15 @@
 public class Model {
+    String modelDescription;
 
+    public Model(String modelDescription) {
+        this.modelDescription = modelDescription;
+    }
+
+    public String getModelDescription() {
+        return modelDescription;
+    }
+
+    public void setModelDescription(String modelDescription) {
+        this.modelDescription = modelDescription;
+    }
 }
